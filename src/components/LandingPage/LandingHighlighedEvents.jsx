b0be8d4cf84185a68732beecaa56720f9c6f2e22
nicholasfong <nfong01@students.cs.ubc.ca>
2023-09-19 12:57:01 -0700

    added photos

--- src/components/LandingPage/LandingHighlighedEvents.jsx
+++ src/components/LandingPage/LandingHighlighedEvents.jsx
@@ -1,49 +1,56 @@
-function LandingHighlightedEvents() {
-    const events = [
-      {
-        id: 1,
-        name: 'Event 1',
-        description: 'Description for Event 1',
-        imageUrl: 'image_url_1.jpg',
-      },
-      {
-        id: 2,
-        name: 'Business Fundamentals workshop',
-        description: 'CAP hosts a Business Fundamentals workshop that introduces attendees to the world of business and professional development, gives them a great opportunity to network with one another, and informs them about the many options that are available to UBC Sauder Students post-graduation',
-        imageUrl: 'image_url_2.jpg',
-      },
-      {
-        id: 3,
-        name: 'CAP Case Competition',
-        description: 'Every year, CAP hosts their very own case competition that gives participants an opportunity to develop and apply their critical thinking and problem-solving skills in a business context.',
-        imageUrl: 'image_url_3.jpg',
-      },
-    ];
-  
-    return (
-      <div className="bg-black pb-20">
+import QandA from "../../assets/EventsPhotos/Event1Photo.jpg";
+import CaseCompetition from "../../assets/EventsPhotos/Event2Photo.jpg";
+import Workshop from "../../assets/EventsPhotos/Event3Photo.jpg";
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {events.map((event) => (
-            <div
-              key={event.id}
-              className="bg-black rounded-lg overflow-hidden shadow-md border-4 border-lime-400"
-            >
-              <img
-                src={event.imageUrl}
-                alt={event.name}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h2 className="text-lg font-bold mb-2 text-lime-400">{event.name}</h2>
-                <p className="text-white">{event.description}</p>
-              </div>
+function LandingHighlightedEvents() {
+  const events = [
+    {
+      id: 1,
+      name: "Q & A",
+      description:
+        "The Campus Ambassador Program is launching a brand new event to help applicants answer all their inquiries. Prospective applicants can attend our virtual Information and Q&A sessions with our panelist from various backgrounds. We will have a plethora of students with unique backgrounds to share their journey to Sauder and answer any of your questions! We welcome all students interested in Sauder to attend.",
+      imageUrl: { QandA },
+    },
+    {
+      id: 2,
+      name: "Business Fundamentals workshop",
+      description:
+        "CAP hosts a Business Fundamentals workshop that introduces attendees to the world of business and professional development, gives them a great opportunity to network with one another, and informs them about the many options that are available to UBC Sauder Students post-graduation",
+      imageUrl: { Workshop },
+    },
+    {
+      id: 3,
+      name: "CAP Case Competition",
+      description:
+        "Every year, CAP hosts their very own case competition that gives participants an opportunity to develop and apply their critical thinking and problem-solving skills in a business context.",
+      imageUrl: { CaseCompetition },
+    },
+  ];
+
+  return (
+    <div className="bg-black pb-20">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {events.map((event) => (
+          <div
+            key={event.id}
+            className="bg-black rounded-lg overflow-hidden shadow-md border-4 border-lime-400"
+          >
+            <img
+              src={event.imageUrl}
+              alt={event.name}
+              className="w-full h-48 object-cover"
+            />
+            <div className="p-4">
+              <h2 className="text-lg font-bold mb-2 text-lime-400">
+                {event.name}
+              </h2>
+              <p className="text-white">{event.description}</p>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  }
-  
-  export default LandingHighlightedEvents;
-  
+    </div>
+  );
+}
+
+export default LandingHighlightedEvents;
